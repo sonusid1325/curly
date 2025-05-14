6b6a26a2022f6fd460e6d71b54d3606692858abd
ソヌ <sonusid1325@gmail.com>
2025-05-14 22:57:58 +0530

    base

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+use clap::{Arg, Command};
+
 fn main() {
-    println!("Hello, world!");
+    let matches = Command::new("rust-curl")
+        .version("1.0")
+        .author("sonusid1325")
+        .about("Stylish HTTP client like curl")
+        .arg(Arg::new("url").required(true).help("The URL to request"))
+        .arg(
+            Arg::new("method")
+                .short('X')
+                .long("request")
+                .default_value("GET")
+                .help("HTTP method to use"),
+        )
+        .get_matches();
+
+    let url = matches.get_one::<String>("url").unwrap();
+    let method = matches.get_one::<String>("method").unwrap();
+
+    println!("Method: {}, URL: {}", method, url);
 }
